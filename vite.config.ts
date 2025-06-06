bc2067b74fd3c291cbf735049e723463632e0250
Elena Castro <elena@aircury.com>
2025-06-06 17:04:07 +0200

    Scaffolding | Added data loader logic

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
+import preact from '@preact/preset-vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
-});
+    plugins: [preact()],
+    resolve: {
+        alias: [
+            { find: 'react', replacement: 'preact/compat' },
+            { find: 'react-dom', replacement: 'preact/compat' },
+
+            {
+                find: '@arcadia/hooks',
+                replacement: path.resolve(__dirname, 'src/hooks'),
+            },
+            {
+                find: '@arcadia/dictionary',
+                replacement: path.resolve(
+                    __dirname,
+                    'src/helpers/dictionary.ts'
+                ),
+            },
+        ],
+    },
+})
