c2a3899cb6a945950060f7b3d25ac91cd917e5c6
Elena Castro <elena@aircury.com>
2025-06-06 16:27:32 +0200

    Scaffolding | Added linter+prettier

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import importPlugin from 'eslint-plugin-simple-import-sort'
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    {
+        files: ['**/*.{ts,tsx}'],
+        plugins: { 'simple-import-sort': importPlugin },
+        rules: {
+            'simple-import-sort/imports': [
+                'error',
+                { groups: [['^react$', '^@?\\w'], ['^@arcadia/*']] },
+            ],
+        },
+    }
+)
